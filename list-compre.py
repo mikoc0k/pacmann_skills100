f29bac420fe96d17b4785cf96280fe957877fa47
mikoc0k <uciharis@hotmail.com>
2025-06-16 20:24:52 +0700

    membuat list baru tanpa list dasar

--- list-compre.py
+++ list-compre.py
@@ -18,3 +18,25 @@ print("cara list compre- : ",daftar_gaji_baru_compre)
 # dengan List comprehension, kita bisa langsung memanipulasi output
 daftar_gaji_bagi_5 = [gaji/5 for gaji in daftar_gaji]
 print("hasil daftar gaji / 5 : ",daftar_gaji_bagi_5)
+
+# memberi syarat lalu mencetak
+gaji_lebih_dari_3000k = [gaji for gaji in daftar_gaji if gaji > 3_000_000]
+print("gaji > 3juta : ",gaji_lebih_dari_3000k)
+
+# contoh lain
+nilai_kelas_a = [55,60,60,77,89,40,44,90]
+nilai_lulus = [nilai for nilai in nilai_kelas_a if nilai > 70]
+print("nilai lulus >70 : ",nilai_lulus)
+
+# membuat list baru tanpa list yang ada sebelumnya
+
+# cara konven
+list_baru = []
+for angka in range(0,10):
+    list_baru.append(angka)
+
+print("list konven : ",list_baru)
+
+# cara list comprehen
+list_baru_comprehen = [angka for angka in range(0,10)]
+print("list komprehen", list_baru_comprehen)
