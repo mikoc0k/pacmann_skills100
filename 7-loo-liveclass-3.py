13774173d2c1f20b436bf0931fa792533fa3d355
mikoc0k <uciharis@hotmail.com>
2025-08-23 22:10:47 +0700

    jawaban 3a 3b

---
+++ 7-loo-liveclass-3.py
@@ -0,0 +1,9 @@
+# 3a
+belanja_surti = [20_000,80_000,40_000,150_500]
+belanja_mima = [31_000, 43_000, 65_000, 105_000]
+belanja_rani = [12_000, 200_000, 61_000,10_000]
+
+# 3b
+# belanja ibu surti
+total_belanja_surti = sum(belanja_surti)
+print("Total belanja ibu surti adalah Rp", total_belanja_surti)
