824c129806724e85e717a63aa23e04fb585b33b5
mikoc0k <uciharis@hotmail.com>
2025-08-25 22:59:12 +0700

    jawaban soal nomor 5

---
+++ 7-loop-liveclass-5.py
@@ -0,0 +1,27 @@
+"""
+nama barang     | jumlah
+sandal angsa    | 4
+mie seleraku    | 15
+kopi putih      | 20
+kopi kapal karam | 12
+sabun kehidupan | 7
+sampo matahari  | 11
+lombok cap gajah | 3
+bawang merah    | 2
+bawang putih    | 3
+kecap EFG      | 6
+"""
+
+daftar_harga = [
+    ['sandal angsa', 4],
+    ['mie seleraku', 15],
+    ['kopi putih', 20],
+    ['kopi kapal karam', 12],
+    ['sabun kehidupan', 7],
+    ['sampo matahari', 11],
+    ['lombok cap gajah', 3],
+    ['bawang merah', 2],
+    ['bawang putih', 3],
+    ['kecap EFG', 6]
+]
+print(daftar_harga)
