e925cab95390120b6590f9b82966f7624db76449
mikoc0k <uciharis@hotmail.com>
2025-08-27 20:53:10 +0700

    section fungsi di python

---
+++ 8-function.py
@@ -0,0 +1,16 @@
+# fungsi = kode yang bisa dipakai berkali kali
+def perkalian(a, b):
+    print(f"{a} x {b} = {a * b}")
+
+perkalian(2, 3)
+perkalian(5, 6)
+perkalian(10, 4)
+
+# DRY = Don't Repeat Yourself
+# ----------------------------------------------- #
+
+# mini exercise
+def fungsi_teks():
+    print('saya sedang belajar fungsi di python')
+
+fungsi_teks()
