641189a01990ac1e65ccf769f75b879df64587e4
mikoc0k <uciharis@hotmail.com>
2025-09-01 12:55:02 +0700

    contoh penggunaan lambda

--- 8-function-lambda.py
+++ 8-function-lambda.py
@@ -1,3 +1,28 @@
-# Lambda adalah fungsi
+# Lambda adalah fungsi tanpa nama (anonymous)
 # jika fungsi didefinisikan dengan def
 # maka lambda didefinisikan dengan lambda
+
+# contoh fungsi lambda
+sapa = lambda nama: print(f"hallo {nama}")
+sapa("farhan kebab")
+
+# format penulisan lambda
+# lambda <params>: <isi fungsi>
+
+# contoh lain
+kuadrat = lambda angka: angka**2 # karna lambda tidak punya nama maka perlu var kuadrat utk simpan nilai
+print(kuadrat(2)) # baris hanya 1, tidak bs lebih
+# ekspresi juga cm 1
+
+# contoh lagi
+profit = lambda income, cost: income - cost
+print(profit(100_000,50_500))
+
+# mini exercise
+harga_diskon = lambda qty,price,disc: qty * price * (1-disc)
+print(harga_diskon(2,1_000_000,0.15))
+
+# fungsi lambda untuk percabangan
+hitung = lambda num: num+1 if num < 10 else 2*num
+print(hitung(10))
+print(hitung(5))
